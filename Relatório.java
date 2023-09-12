45b279d8fb92ef0da77fedbd236ef9471e19ef9a
Rebeca Nunes <rebecaviananunes@gmail.com>
2023-09-12 09:11:08 -0300

    Update Relatório.java

--- Relatório.java
+++ Relatório.java
@@ -1,35 +1,18 @@
 
-public class Faturamento {
+public class Relat�rio {
 	
-	public static void main (String [] args) {
+	public static void main (String []args) {
 		
-		double precoSucoDeLaranja = 10.00;
-		double precoSucoDeUva = 8.00;
-		double precoSucoDeCaju = 6.45;
-	
+		String nomeDoDiretorDoSenaiDeMogi = "Itamar Cruz";
+		String unidadeMogi = " CFP 1.17";
+		String nomeDoCoordenador = "Wagner Cunha";
 		
-		int unidadeSucoDeLaranja = 200;
-		int unidadeSucoDeUva = 30;
-		int unidadeSucoDeCaju = 150;
-		
-		double totalDeVendas = unidadeSucoDeLaranja * precoSucoDeLaranja + precoSucoDeUva * unidadeSucoDeUva + precoSucoDeCaju * unidadeSucoDeCaju;
+		System.out.println("Relat�rio");
+		System.out.println("Diretor Senai Mogi: " + nomeDoDiretorDoSenaiDeMogi);
+		System.out.println("Unidade:" + unidadeMogi);
+		System.out.println("Coordenador: " + nomeDoCoordenador);
 		
 		
-		System.out.println("Relat�rio de Faturamento: ");
-		System.out.println("                          ");
-		System.out.println("Produto/Pre�o:");
-		System.out.println("Pre�o do suco de Laranja: R$" + precoSucoDeLaranja);
-		System.out.println("Pre�o do suco de Uva: R$" + precoSucoDeUva);
-		System.out.println("Pre�o do suco de Caj�: R$" + precoSucoDeCaju);
-		System.out.println("-------------------------------------");
-		System.out.println("Faturamento:");
-		System.out.println(unidadeSucoDeLaranja + " unidades de suco de Laranja: R$" + precoSucoDeLaranja * unidadeSucoDeLaranja);
-		System.out.println( unidadeSucoDeUva + " unidades de suco de Uva: R$" + precoSucoDeUva * unidadeSucoDeUva);
-		System.out.println(unidadeSucoDeCaju + " unidades de suco de caj�: R$" + precoSucoDeCaju * unidadeSucoDeCaju);
-		System.out.println("                                       ");
-		System.out.println("Total de vendas: R$" + totalDeVendas);
 		
-	
 	}
-		
 }
